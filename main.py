1a2f1f396a45509e101231fe7c158ed035507d29
Igor Vorobyev <67874208+Alekcey-Golybev9090@users.noreply.github.com>
2021-11-22 15:56:56 +0300

    last solution

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randrange
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.btn_create.clicked.connect(self.create_circle)
+
+    def create_circle(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(255, 242, 0))
+            r = randrange(5, 200)
+            qp.drawEllipse(100, 150, 2 * r, 2 * r)
+            qp.end()
+            self.do_paint = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWindow()
+    ex.show()
+    sys.exit(app.exec_())
